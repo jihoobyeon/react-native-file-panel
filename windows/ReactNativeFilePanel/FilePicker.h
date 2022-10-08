e52c0db4e29b4a0714dca64ffb65c0790c21b4c6
Jihoo Byeon <jihoobyeon@gmail.com>
2022-10-08 19:02:24 +0900

    Update FilePicker.h

--- windows/ReactNativeFilePanel/FilePicker.h
+++ windows/ReactNativeFilePanel/FilePicker.h
@@ -18,6 +18,11 @@ using namespace Windows::UI::Xaml;
 
 namespace FilePicker
 {
+	FilePicker::FilePicker()
+	{
+        	InitializeComponent();
+	}
+	
 	REACT_MODULE(Panel);
 	struct Panel
 	{
@@ -26,17 +31,19 @@ namespace FilePicker
 		REACT_METHOD(Open, L"open");
 		fire_and_forget Open(wchar_t ext[], React::ReactPromise<string>&& result) noexcept
 		{
+			auto lifetime = get_strong();
+			
 			FileOpenPicker openPicker;
 			openPicker.ViewMode(PickerViewMode::Thumbnail);
 			openPicker.SuggestedStartLocation(PickerLocationId::DocumentsLibrary);
-			openPicker.FileTypeFilter().ReplaceAll({ wcscat(dot, ext) });
+			openPicker.FileTypeFilter().ReplaceAll({ wcscat_s(dot, 32, ext) });
 
 			StorageFile file = co_await openPicker.PickSingleFileAsync();
 
 			if (file == nullptr) {
-				result.Reject();
+				result.Reject("No file selected.");
 			} else {
-				string uri = file.Path;
+				string uri = file.Path();
 				result.Resolve(uri);
 			}
 		}
@@ -44,9 +51,11 @@ namespace FilePicker
 		REACT_METHOD(Save, L"save");
 		fire_and_forget Save(wchar_t ext[], wchar_t content[]) noexcept
 		{
+			auto lifetime = get_strong();
+			
 			FileSavePicker savePicker;
 			savePicker.SuggestedStartLocation(PickerLocationId::DocumentsLibrary);
-			savePicker.FileTypeChoices().Insert(L"", single_threaded_vector<hstring>({ wcscat(dot, ext) }));
+			savePicker.FileTypeChoices().Insert(L"", single_threaded_vector<hstring>({ wcscat_s(dot, 32, ext) }));
 
 			StorageFile file = co_await savePicker.PickSaveFileAsync();
 
