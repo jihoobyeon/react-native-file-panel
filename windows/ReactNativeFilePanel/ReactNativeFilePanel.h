54f5992a66937d5cc18c44ded445335fc97957a2
Jihoo Byeon <jihoobyeon@gmail.com>
2022-10-09 22:00:59 +0900

    Update ReactNativeFilePanel.h

--- windows/ReactNativeFilePanel/ReactNativeFilePanel.h
+++ windows/ReactNativeFilePanel/ReactNativeFilePanel.h
@@ -31,7 +31,7 @@ namespace FilePicker
             FileOpenPicker openPicker;
             openPicker.ViewMode(PickerViewMode::List);
             openPicker.SuggestedStartLocation(PickerLocationId::DocumentsLibrary);
-            openPicker.FileTypeFilter().Add(str);
+            openPicker.FileTypeFilter().Append(str);
             
             StorageFile file = co_await openPicker.PickSingleFileAsync();
             if (file == nullptr) {
@@ -49,7 +49,7 @@ namespace FilePicker
             
             FileSavePicker savePicker;
             savePicker.SuggestedStartLocation(PickerLocationId::DocumentsLibrary);
-            savePicker.FileTypeChoices().Add("", { to_string(str) });
+            savePicker.FileTypeChoices().Insert(0, { to_string(str) });
 
             StorageFile file = co_await savePicker.PickSaveFileAsync();
             await FileIO::WriteTextAsync(file, content.c_str());
