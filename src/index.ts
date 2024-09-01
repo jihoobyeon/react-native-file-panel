98039681985463edb17319935c64d517ff4a210f
Jihoo Byeon <jihoobyeon@gmail.com>
2024-09-01 10:36:10 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,44 @@
 const FilePanel = require('./NativeFilePanel').default;
 
-export function openFile(ext?: string | string[]): Promise<string> {
-  if (ext) {
-    if (typeof ext === 'string') return FilePanel.openFile([ext]);
-    else return FilePanel.openFile(ext);
-  } else return FilePanel.openFile(['*']);
+export function openFile(ext?: string | string[], defaultPath?: string): Promise<string> {
+  if (defaultPath) {
+    if (ext) {
+      if (typeof ext === 'string') return FilePanel.openFile([ext], defaultPath);
+      else return FilePanel.openFile(ext, defaultPath);
+    } else return FilePanel.openFile(['*'], defaultPath);
+  } else {
+    if (ext) {
+      if (typeof ext === 'string') return FilePanel.openFile([ext], '*');
+      else return FilePanel.openFile(ext, '*');
+    } else return FilePanel.openFile(['*'], '*');
+  }
 }
 
-export function openFiles(ext?: string | string[]): Promise<string[]> {
-  if (ext) {
-    if (typeof ext === 'string') return FilePanel.openFiles([ext]);
-    else return FilePanel.openFiles(ext);
-  } else return FilePanel.openFiles(['*']);
+export function openFiles(ext?: string | string[], defaultPath?: string): Promise<string[]> {
+  if (defaultPath) {
+    if (ext) {
+      if (typeof ext === 'string') return FilePanel.openFiles([ext], defaultPath);
+      else return FilePanel.openFiles(ext, defaultPath);
+    } else return FilePanel.openFiles(['*'], defaultPath);
+  } else {
+    if (ext) {
+      if (typeof ext === 'string') return FilePanel.openFiles([ext], '*');
+      else return FilePanel.openFiles(ext, '*');
+    } else return FilePanel.openFiles(['*'], '*');
+  }
 }
 
-export function openFolder(): Promise<string> {
-  return FilePanel.openFolder();
+export function openFolder(defaultPath?: string): Promise<string> {
+  if (defaultPath) return FilePanel.openFolder(defaultPath);
+  else return FilePanel.openFolder('*');
 }
 
-export function saveFile(ext: string | string[], content: string): Promise<void> {
-  if (typeof ext === 'string') return FilePanel.saveFile([ext], content);
-  else return FilePanel.saveFile(ext, content);
+export function saveFile(ext: string | string[], content: string, defaultPath?: string): Promise<string> {
+  if (defaultPath) {
+    if (typeof ext === 'string') return FilePanel.saveFile([ext], content, defaultPath);
+    else return FilePanel.saveFile(ext, content, defaultPath);
+  } else {
+    if (typeof ext === 'string') return FilePanel.saveFile([ext], content, '*');
+    else return FilePanel.saveFile(ext, content, '*');
+  }
 }
