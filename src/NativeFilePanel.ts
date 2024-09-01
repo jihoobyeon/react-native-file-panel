d12c2f8097d7d49d7d8733dbfc6194320a748b2c
Jihoo Byeon <jihoobyeon@gmail.com>
2024-09-01 10:54:35 +0900

    Update NativeFilePanel.ts

--- src/NativeFilePanel.ts
+++ src/NativeFilePanel.ts
@@ -2,10 +2,10 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  openFile(ext: string[], defaultPath: string): Promise<string>;
-  openFiles(ext: string[], defaultPath: string): Promise<string[]>;
-  openFolder(defaultPath: string): Promise<string>;
-  saveFile(ext: string[], content: string, defaultPath: string): Promise<string>;
+  openFile(ext: string[]): Promise<string>;
+  openFiles(ext: string[]): Promise<string[]>;
+  openFolder(): Promise<string>;
+  saveFile(ext: string[], content: string): Promise<string>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('FilePanel');
